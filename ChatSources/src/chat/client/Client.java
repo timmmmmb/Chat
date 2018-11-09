5ff9adbd0b8a2619975e1fdcd3d1621d2b190472
Tim Frey <frey.tim@outlook.com>
2018-11-09 13:36:47 +0100

    followed the first 3 steps of the tutorial

--- ChatSources/src/chat/client/Client.java
+++ ChatSources/src/chat/client/Client.java
@@ -1,4 +1,17 @@
 package chat.client;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
+    public static void main(String[] args){
+        Socket socket = null;
+        int portNumber = 4444;
+        try{
+            socket = new Socket("localhost", portNumber);
+        }catch(IOException e){
+            System.err.println("Fatal Connection Error!");
+            e.printStackTrace();
+        }
+    }
 }
--- ChatSources/src/chat/server/ChatServer.java
+++ ChatSources/src/chat/server/ChatServer.java
@@ -1,4 +1,29 @@
 package chat.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ChatServer {
+    private static ServerSocket serverSocket = null;
+    private static int portNumber = 4444;
+    public static void main(String[] args){
+        try{
+            serverSocket = new ServerSocket(portNumber);
+            acceptClients();
+        }catch(IOException e){
+            System.err.println("Could not listen on Port: "+portNumber);
+            System.exit(0);
+        }
+    }
+
+    private static void acceptClients() {
+        while(true){
+            try{
+                Socket socket = serverSocket.accept();
+            }catch(IOException e){
+                System.err.println("Accept failed on port: "+portNumber);
+            }
+        }
+    }
 }
